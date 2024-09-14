bb3f202a40448448521f084bcdbd0927d4eac2b3
vker <weidonghao@zuoyebang.com>
2024-09-14 10:46:47 +0800

    add lua style cfg

--- wezterm.lua
+++ wezterm.lua
@@ -14,8 +14,6 @@ local config = {}
 
 config.leader = { key = "Space", mods = "CTRL|SHIFT" }
 
-fonts.apply_to_config(config)
-
 -- 主题配色
 config.color_scheme_dirs = { "~/.config/wezterm/colors" }
 -- config.color_scheme = "Catppuccin Mocha"
@@ -36,24 +34,14 @@ config.initial_rows = 24
 config.default_prog = { "nu" }
 
 -- 配置gpu加速
--- for _, gpu in ipairs(wezterm.gui.enumerate_gpus()) do
--- 	if gpu.backend == "Vulkan" and gpu.device_type == "IntegratedGpu" then
--- 		config.webgpu_preferred_adapter = gpu
--- 		config.front_end = "WebGpu"
--- 		break
--- 	end
--- end
 config.webgpu_preferred_adapter = gpus[1]
 config.front_end = "WebGpu"
 config.animation_fps = 60
 config.max_fps = 60
 
+fonts.apply_to_config(config)
 keybinding.apply_to_config(config)
 key_tables.apply_to_config(config)
 hyperlink.apply_to_config(config)
-
--- wezterm.on("window-config-reloaded", function(_window, _pane)
--- 	wezterm.log_info("the config was reloaded for this window")
--- end)
 
 return config
